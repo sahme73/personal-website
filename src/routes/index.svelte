e3a2319a050e0e67c17e91e77d2372cfeb8d8d53
Safeer Ahmed <19165518a@gmail.com>
2022-07-04 00:56:26 -0500

    Added basic information to the homepage.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let name = 'Safeer Ahmed';
+</script>
+
+<h1>Welcome to {name}'s Website!</h1>
+
+<p>
+    This website is powered by SvelteKit!<br>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read documentation about this website's framework.
+</p>
