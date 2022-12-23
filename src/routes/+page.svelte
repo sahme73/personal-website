1f61d2834304742e932bf66aaf2dd75a1086d331
Safeer Ahmed <sahme73@illinois.edu>
2022-12-23 00:32:52 -0600

    Migrated old pages to updated web app.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Random from './temp/random.svelte';
+    let name = 'Safeer Ahmed';
+</script>
+
+<h1>Welcome to {name}'s Website!</h1>
+
+<p>
+    This website is powered by the latest version SvelteKit!<br>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read documentation about this website's framework.
+</p>
+
+<Random/>
---
+++ src/routes/temp/random.svelte
@@ -0,0 +1,31 @@
+<script>
+	let user = { loggedIn: false };
+	let status = '';
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+	
+	$: {
+		if (user.loggedIn)
+			status = 'logged in';
+		else
+		  status = 'logged out';
+	}
+	
+</script>
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{/if}
+
+{#if !user.loggedIn}
+	<button on:click={toggle}>
+		Log in
+	</button>
+{/if}
+
+<p>
+	Your are currently {status};
+</p>
