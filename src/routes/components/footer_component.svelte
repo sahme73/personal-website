20a50554615e4bcf9bb967a4db180fdc69c20c54
Safeer Ahmed <sahme73@illinois.edu>
2023-01-02 03:33:39 -0600

    Links now open in new tabs and are secure against (reverse) tabnabbing phishing attempts.

--- src/routes/components/footer_component.svelte
+++ src/routes/components/footer_component.svelte
@@ -1,11 +1,11 @@
 <footer>
     <p>
-        Connect: <a href="https://www.linkedin.com/in/safeer-ahmed-uiuc/">LinkedIn</a>
-         | <a href="https://www.github.com/sahme73/">GitHub</a>
+        Connect: <a href="https://www.linkedin.com/in/safeer-ahmed-uiuc/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+         | <a href="https://www.github.com/sahme73/" target="_blank" rel="noopener noreferrer">GitHub</a>
     </p>
 
     <p>
         This website is powered by the latest version SvelteKit!<br>
-        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read documentation about this website's framework.
+        Visit <a href="https://kit.svelte.dev" target="_blank" rel="noopener noreferrer">kit.svelte.dev</a> to read documentation about this website's framework.
     </p>
 </footer>
