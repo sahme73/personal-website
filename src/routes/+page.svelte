c17f90a2436b4dd980d0a10be991c035ed0ce90a
Safeer Ahmed <sahme73@illinois.edu>
2023-01-02 03:25:32 -0600

    Added button to download resume. Added GitHub profile link. Fixed some formatting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script>
     import Component1 from './components/component1.svelte';
     import Component2 from './components/component2.svelte';
+    import ResumeComponent from './components/resume_component.svelte';
     import FooterComponent from './components/footer_component.svelte';
     let name = 'Safeer Ahmed';
 </script>
 
-<h1>Welcome to {name}'s Website!</h1>
+<h1>Welcome to {name}'s Incomplete Website!</h1>
 
-<p>
-    This website is powered by the latest version SvelteKit!<br>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read documentation about this website's framework.
-</p>
-
+<ResumeComponent/>
+<br><br>
 <Component1/>
 <Component2/>
 <FooterComponent/>
--- src/routes/components/footer_component.svelte
+++ src/routes/components/footer_component.svelte
@@ -1,5 +1,11 @@
 <footer>
     <p>
         Connect: <a href="https://www.linkedin.com/in/safeer-ahmed-uiuc/">LinkedIn</a>
+         | <a href="https://www.github.com/sahme73/">GitHub</a>
+    </p>
+
+    <p>
+        This website is powered by the latest version SvelteKit!<br>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read documentation about this website's framework.
     </p>
 </footer>
